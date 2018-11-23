caa8a42efe5c116e5799ab75d88ab0302a89ab20
3A532055 <ylhc214044@gmail.com>
2018-11-23 14:52:17 +0800

    建立User&Task models之間的關聯

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
